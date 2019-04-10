a9da422300c689a0b4ee6bb681bc2af6ad33d4c1
Keilane <keilanerizza@gmail.com>
2019-04-10 20:05:18 -0300

    atributos da classe cliente

--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,2 +1,11 @@
 export class Cliente {
+    codigo: number;
+    nome: string;
+    cargo: string;
+    endereco: string;
+    cidade: string;
+    cep: string;
+    pais: string;
+    telefone: string;
+    fax: string;
 }
