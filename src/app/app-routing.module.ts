a7610b5ab73b1adb1f445d2db9103bbad8c4036a
Keilane <keilanerizza@gmail.com>
2019-04-10 17:43:58 -0300

    rota componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [];
 
@@ -7,4 +8,11 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent }
+  ];
+
+}
