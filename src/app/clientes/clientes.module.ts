521a940caf43990efc68191b60d2b3630c7569d0
Keilane <keilanerizza@gmail.com>
2019-04-10 19:42:55 -0300

    criando componente de listar cliente

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClienteListComponent } from './cliente-list/cliente-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ClienteListComponent],
   imports: [
     CommonModule
   ]
