67cbd7a4a97081797c1b38fd1a3335a7e35732f6
Keilane <keilanerizza@gmail.com>
2019-04-10 22:08:54 -0300

    listando clientes

--- src/app/clientes/cliente-list/cliente-list.component.ts
+++ src/app/clientes/cliente-list/cliente-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-cliente-list',
@@ -9,7 +10,32 @@ export class ClienteListComponent implements OnInit {
 
   constructor() { }
 
+  clientes: Cliente[] = [];
+
   ngOnInit() {
+    this.clientes = [{
+      'codigo': 1,
+      'nome': 'Carlos',
+      'cargo': 'Professor',
+      'endereco': 'Rua teste, 65, Jardim das Palmeiras',
+      'cidade': 'Uberlandia',
+      'cep': '38400-000',
+      'pais': 'Brasil',
+      'telefone': '34944423402',
+      'fax': '343434344'
+    },
+    {
+      'codigo': 2,
+      'nome': 'Martin Fowler',
+      'cargo': 'CEO',
+      'endereco': '40, street view, google',
+      'cidade': 'Miami',
+      'cep': '30111',
+      'pais': 'USA',
+      'telefone': '55100912333',
+      'fax': '232323'
+    },
+    ]
   }
 
 }
