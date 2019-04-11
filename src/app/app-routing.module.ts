4f17cb71eee410d3465b2e2cd39c9826efb6105e
Keilane <keilanerizza@gmail.com>
2019-04-10 22:06:30 -0300

    correcao do arquivo de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  // { path: 'clientes', loadChildren: 'app/clientes/clientes.module#ClientesModule' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-
-  routes = [
-    { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent }
-  ];
-
 }
