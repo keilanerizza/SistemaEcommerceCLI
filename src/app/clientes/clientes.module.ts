b2192eb3bf4edacc30d463457086b4796d41a00c
Keilane <keilanerizza@gmail.com>
2019-04-10 22:10:14 -0300

    import ClienteRoutingModule

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
+import { ClientesRoutingModule } from './clientes-routing.module';
 
 @NgModule({
   declarations: [ClienteListComponent],
   imports: [
-    CommonModule
+    CommonModule, ClientesRoutingModule, FormsModule
   ]
 })
 export class ClientesModule { }
