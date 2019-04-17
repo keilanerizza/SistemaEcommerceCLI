a46743d7e99e44cb67d710906b9502e11e05c8cd
Keilane <keilanerizza@gmail.com>
2019-04-17 19:50:14 -0300

    Correcao rotas

--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClienteListComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ClientesRoutingModule {
-
-  routes = [
-    { path: '', component: ClienteListComponent}
-  ];
-
-}
+export class ClientesRoutingModule {}
