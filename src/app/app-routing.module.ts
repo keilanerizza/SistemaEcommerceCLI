2168610e55eea11135c51d9a964325976162994a
Keilane <keilanerizza@gmail.com>
2019-04-21 19:22:08 -0300

    correcao erro importacao modulo de cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'clientes',loadChildren: 'app/clientes/clientes.module#ClientesModule' }
+  { path: 'clientes', loadChildren: './clientes/clientes.module#ClientesModule' }
 ];
 
 @NgModule({
