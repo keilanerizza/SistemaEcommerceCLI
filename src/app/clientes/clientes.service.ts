096cab627a42f73993299a5e36ef46765bd97b48
Keilane <keilanerizza@gmail.com>
2019-04-21 19:44:23 -0300

    metodos add e remove cliente.service

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Cliente } from './cliente';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +14,33 @@ export class ClientesService {
 
   private url: string = 'http://localhost:8080/clientes';
 
+  clientesChanged = new EventEmitter<Observable<Cliente[]>>();
+
   constructor(private http: Http) { }
 
   getAll(): Observable<Cliente[]> {
-    return this.http.get(this.url).map(res => res.json()).catch(this.handleError);
+    return this.http.get(this.url)
+    .map(res => res.json())
+    .catch(this.handleError);
   }
 
   private handleError(error: any) {
     let erro = error.message || 'Server error';
     console.error('Ocorreu um erro', error);
     return Observable.throw(erro);
+  }
+
+  add(cliente: Cliente) {
+    return this.http.post(this.url,JSON.stringify(cliente),
+    {headers: this.getHeaders()})
+    .do(data => this.clientesChanged.emit(this.getAll()))
+    .catch(this.handleError);
+  }
+
+  remove(id: number) {
+    return this.http.delete(this.getUrl(id), {headers: this.getHeaders()})
+    .map(res => res.json())
+    .do(data => this.clientesChanged.emit(this.getAll()))
+    .catch(this.handleError);
   }
 }
