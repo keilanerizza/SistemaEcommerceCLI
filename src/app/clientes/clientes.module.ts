3e74a3472a624fd9b449e427703083fd9c86f44d
Keilane <keilanerizza@gmail.com>
2019-04-21 19:21:40 -0300

    componente cliente-form

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { ClientesRoutingModule } from './clientes-routing.module';
+import { ClienteFormComponent } from './cliente-form/cliente-form.component';
 
 @NgModule({
-  declarations: [ClienteListComponent],
+  declarations: [ClienteListComponent, ClienteFormComponent],
   imports: [
     CommonModule, ClientesRoutingModule, FormsModule
   ]
