57ac85e4a79986e9f6066230e025a874c2988c39
Keilane <keilanerizza@gmail.com>
2019-05-29 21:31:56 -0300

    modulo autores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'autores', loadChildren: './autores/autores.module#AutoresModule' },
   { path: 'clientes', loadChildren: './clientes/clientes.module#ClientesModule' }
 ];
 
