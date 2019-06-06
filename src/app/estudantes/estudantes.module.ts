7512cf951273fe9dcea2268ed50cafcd306550c4
Keilane <keilanerizza@gmail.com>
2019-06-05 21:52:20 -0300

    componente estudante-list

--- src/app/estudantes/estudantes.module.ts
+++ src/app/estudantes/estudantes.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EstudanteListComponent } from './estudante-list/estudante-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EstudanteListComponent],
   imports: [
     CommonModule
   ]
