48e60eea81450d4a6bea4cfaaec67be1cc987aca
Keilane <keilanerizza@gmail.com>
2019-06-06 19:22:57 -0300

    modulo livros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'livros', loadChildren: './livros/livros.module#LivrosModule' },
   { path: 'autores', loadChildren: './autores/autores.module#AutoresModule' },
   { path: 'clientes', loadChildren: './clientes/clientes.module#ClientesModule' },
   { path: 'estudantes', loadChildren: './estudantes/estudantes.module#EstudantesModule' }
