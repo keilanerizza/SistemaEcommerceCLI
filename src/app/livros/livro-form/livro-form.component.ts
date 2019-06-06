e2858ff130e69d0a413452465bb0a13ac0f503e8
Keilane <keilanerizza@gmail.com>
2019-06-06 19:29:27 -0300

    componente livro-form

---
+++ src/app/livros/livro-form/livro-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-form',
+  templateUrl: './livro-form.component.html',
+  styleUrls: ['./livro-form.component.sass']
+})
+export class LivroFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/livros/livros.module.ts
+++ src/app/livros/livros.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LivroFormComponent } from './livro-form/livro-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LivroFormComponent],
   imports: [
     CommonModule
   ]
